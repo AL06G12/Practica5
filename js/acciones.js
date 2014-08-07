346ec9187016787bd25e75520d97311caba78bb1
Marion Itzel Martinez Ortega <marion.itzel@hotmail.com>
2014-08-07 09:41:05 -0500

    Version 1.1.2

--- js/acciones.js
+++ js/acciones.js
@@ -9,14 +9,18 @@ $(document).ready(function(e){
 	
 		audio=window.plugins.LowLatencyAudio;
 		audio.preloadFX('do','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
-		audio.preloadFX('re','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
-		audio.preloadFX('mi','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
-		audio.preloadFX('fa','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
-		audio.preloadFX('sol','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
-		audio.preloadFX('la','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
-		audio.preloadFX('si','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('re','audio/D.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('mi','audio/E.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('fa','audio/F.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('sol','audio/G.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('la','audio/A.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('si','audio/B.mp3',function(msg){},function(msg){alert ('error: '+msg);});
 		
-		
+	$('.nota').bind('touchstart',function(e){
+		$(this).addClass('tocada');
+		audio.play($(this).attr('id'));
+		}).bind('touchend',function(e){
+			$(this).removeClass('tocada');})	
 	
 	
 	});//deciceready
